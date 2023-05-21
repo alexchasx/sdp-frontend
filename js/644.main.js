2eaba0e38775490e2e4566ccaae0db96c42c477b
Alexander Chasovnikov <a.s.chasovnikov@yandex.ru>
2023-05-21 20:39:04 +0300

    done work

---
+++ js/644.main.js
@@ -0,0 +1,1 @@
+(self.webpackChunksdp_frontend=self.webpackChunksdp_frontend||[]).push([[644],{644:()=>{new Swiper(".js-swiper-hero",{slidesPerView:1,loop:!0,pagination:{el:".js-swiper-pagination-hero",type:"bullets",clickable:!0}})}}]);
